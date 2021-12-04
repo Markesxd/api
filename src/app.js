065a1d20f0d7f1a6acc25518e807709dad7f9ea9
Rafael <markesxd@gmail.com>
2021-12-03 21:58:20 -0300

    configs

---
+++ src/app.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+const port = process.env.PORT;
+
+const app = express();
+app.use(express.json());
+
+app.listen(port, () => console.log(`server runnig on port ${port}`));
