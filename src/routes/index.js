e56c4151dc74f6d17eb8fd73e275a6924efb8928
Rafael <markesxd@gmail.com>
2021-12-13 08:53:37 -0300

    add /

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ const report = require('./reportRouts');
 const doctor = require('./doctorRouts');
 
 router
+.get('/', (req, res) => res.status(403).send('<h1>Fuck off</h2>'))
 .use('/paciente', patient)
 .use('/atendimento', attendance)
 .use('/relatorio', report)
