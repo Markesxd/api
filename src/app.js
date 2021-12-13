9ac646aa93e0e83836c19251457b0b1f0ca28fec
Rafael <markesxd@gmail.com>
2021-12-13 07:45:28 -0300

    fixing cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const port = process.env.PORT;
 const router = require('./routes');
 const { errorHandler } = require('./middlewares');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(errorHandler);
